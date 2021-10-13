97ed0f252acfa0bcf27ae81bc192e1abf27f2712
xlite00 <csskishida@gmail.com>
2021-10-13 21:28:55 +0900

    ヘッダ追加

--- src/features/layout/Layout.tsx
+++ src/features/layout/Layout.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Layout: React.FC = ({ children }) => {
-  return <div className="container mx-auto">{children}</div>;
+  return <>
+  <div className="h-10 w-full pl-10 pt-2 top-0 bg-blue-200 fixed">My React Apps</div>
+  <div className="container mx-auto mt-10">{children}</div>
+  </>;
 };
 export default Layout;
